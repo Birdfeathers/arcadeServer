d86c4fc54b1fc4c38ce183909952a9eda9cf7454
Rebecca Carr <reca9238@colorado.edu>
2022-01-21 14:12:24 -0700

    added requireUser and morgan to api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,34 @@
 const express = require("express");
 const router = express.Router();
+// create logs for everything
+const morgan = require('morgan');
+router.use(morgan('dev'));
+const { getUserById } = require('../db/index');
+
+router.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header('Authorization');
+
+  // goes onto the next function if auth is falsey
+  if (!auth) {
+      next();
+  } else if (auth.startsWith(prefix)) { // verifies the token if "auth" starts with "Bearer"
+      const token = auth.slice(prefix.length);
+
+      try {
+          const { id } = await jwt.verify(token, JWT_SECRET);
+
+          if (id) {
+              req.user = await getUserById(id);
+              next();
+          }
+      } catch (error) {
+          next(error)
+      }
+  } else {
+      next(`Authorization token must start with ${ prefix }`)
+  }
+})
 
 router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
