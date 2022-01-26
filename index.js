9cd274f8308651ba93ab3c8b6238cc70322a7815
Rebecca Carr <reca9238@colorado.edu>
2022-01-26 16:55:48 -0700

    cleaned up

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ const io = require("socket.io")(server, {
     }
   });
 
-  let interval;
+
 
 io.on("connection", (socket) => {
-  console.log("New client connected");
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
+  socket.on('move', (move) => {
+    io.emit("game" + move.game, move);
+  })
+})
+
+
 
 const { client } = require('./db');
 
