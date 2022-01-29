0274bb268b98b2d0b303ee53ed14de22c23bb054
Rebecca Carr <reca9238@colorado.edu>
2022-01-29 11:05:15 -0700

    added start and end to lines

--- db/findLines.js
+++ db/findLines.js
@@ -59,22 +59,6 @@ function isOnTable(row, col, rows, cols)
     return true;
 }
 
-function returnLeftNode(node) {
-    node.col = node.col -1;
-    return node;
-}
-
-function returnRightNode(node) {
-    node.col = node.col  + 1;
-    return node;
-}
-
-function returnUpNode(node){
-    node.row = node.row - 1;
-    return node;
-}
-
-
 
 function findLine(root, board, rows, cols, lineNum, linetype)
 {
@@ -87,6 +71,7 @@ function findLine(root, board, rows, cols, lineNum, linetype)
         board[currentNode.row][currentNode.col][linetype] = lineNum;
         currentNode = iterateLine(linetype, currentNode)
     }
+    const start = iterateLine(linetype, currentNode, false);
   
     currentNode = iterateLine(linetype, Object.assign({}, root), false);
     while(getTableVar(currentNode, rows, cols, board).type === color ) 
@@ -95,8 +80,8 @@ function findLine(root, board, rows, cols, lineNum, linetype)
         board[currentNode.row][currentNode.col][linetype] = lineNum;
         currentNode = iterateLine(linetype, currentNode, false);
     }
-
-    return {length, color, lineNum, linetype};
+    const end = iterateLine(linetype, currentNode);
+    return {length, color, lineNum, linetype, start, end};
 }
 
 
@@ -125,55 +110,4 @@ function findAllLines(moveHistory, rows, cols)
 }
 
 module.exports = findAllLines;
-
-// let moveHistory = [
-//     {row: 4, col: 6},
-//     {row: 2, col: 3},
-//     {row: 4, col: 9},
-//     {row: 1, col: 5},
-//     {row: 4, col: 8},
-//     {row: 1, col: 3},
-//     {row: 4, col: 5},
-//     {row: 1, col: 2},
-//     {row: 4, col: 7},
-//     {row: 3, col: 7},
-//     {row: 5, col: 6},
-//     {row: 1, col: 1},
-//     {row: 3, col: 6},
-//     {row: 1, col: 0}
-
-// ]
-
-
-
-
-
-// function createBlankArray(rows, cols){
-//     let arr = [];
-//     for(let i = 0; i < rows; i++)
-//     {
-//         let r = [];
-//         for(let j = 0; j < cols; j++)
-//         {
-//             r.push({occupied:false, type: "none"})
-//         }
-//         arr.push(r);
-//     }
-//     return arr;
-// }
-
-// function createFilledArray(rows, cols, history)
-// {
-//     let arr1 = createBlankArray(rows, cols);
-//     history.forEach((move, indx) => {
-//         let turn;
-//         if(indx % 2 == 0) turn = "black";
-//         else turn = "white";
-//         arr1[move.row][move.col] = {occupied:true, type: turn, moveNum: indx + 1};
-//     });
-//     return arr1;
-// }
-
-//let board = createFilledArray(15, 15, moveHistory);
-// console.log(findAllLines(moveHistory, 15, 15, board));
 
